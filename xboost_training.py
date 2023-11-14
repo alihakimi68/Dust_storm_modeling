455aede45750bb5df25dd3352e354bf9afe724ca
alihakimi68 <120417588+alihakimi68@users.noreply.github.com>
2023-11-14 09:42:33 +0100

    Default parameters

--- xboost_training.py
+++ xboost_training.py
@@ -48,7 +48,7 @@ os.chdir("D:/University/DustStorming/ToAli/DustStormModeling/For training/")
 # shapefile_path = r"D:/ZPX/Research/DustStorm/Thesis files/Input data/For training/dust_sources/dust_training_" + str(year)+ ".shp"
 
 ###################################################################
-# #### Create Data set ############################################
+# #### Default parameters #########################################
 ###################################################################
 
 CreateDataSet = True  # True for creating a dataset from For training folder
